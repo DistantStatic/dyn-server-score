8d1a42925dae6a80f03ecbbfce4a3bddd05dcbdf
distant <oscar.rodr031@gmail.com>
2021-01-09 20:55:50 -0500

    refactors galor and new data system

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, Treemap, HexbinSeries, HeatmapSeries, MarkSeries, LineSeries, LabelSeries, HorizontalBarSeries, VerticalBarSeries, PolygonSeries} from 'react-vis';
+import {
+    Card,
+    CardBody,
+} from "reactstrap";
 import { ResponsiveBubble } from '@nivo/circle-packing'
-import test from "./test.json" 
-import dump from "./dump.json"
-import pdump from "./dump_player.json"
-import sdump from "./dump_skynet.json"
-import { Button } from "bootstrap";
 import PlayerSearchModal from "./searchModal"
+import Papa_Dump from './NewData.json'
 
-const dataaa = { "asdf": 1234};
 const dynadata = { "Al Qusayr" :{"amt":67.304099121099,"co":0,"fc":"000000006E24ED00","id":2,"v":false}, "Aleppo" :{"amt":513.549,"co":0,"fc":"0000000DF582FF50","id":23,"v":false},"Bassel Al-Assad":{"amt":786.18481982422,"co":0,"fc":"0000000DF5812270","id":17,"v":false},"Hama":{"amt":765.55572253418,"co":0,"fc":"000000006E273010","id":10,"v":false},"Hatay":{"amt":72.886288574219,"co":0,"fc":"000000006E2663B0","id":11,"v":false},"Incirlik":{"amt":569.508,"co":0,"fc":"000000006E2685C0","id":12,"v":false},"Jirah":{"amt":100,"co":0,"fc":"0000000DF58188A0","id":13,"v":false},"Minakh":{"amt":25,"co":0,"fc":"0000000DF5836580","id":22,"v":false}}
 const colorSchemes = {
     "blue": "blues",
@@ -18,12 +16,11 @@ const colorSchemes = {
 class App extends Component {
     
     constructor(props) {
-        super()
+        super(props)
         this.state = {
             searchModal: false,
             playerDump: {},
-            dataDump: [],
-            skynetDump: {},
+            dataDump: [], //players
             fuelDataJSON: {},
             mainDataChart: [],
             chartCircleColorBy: "depth", //depth or name
@@ -47,44 +44,45 @@ class App extends Component {
     }
       
     componentDidMount = () => {
-        this.giveBlueBaseStrength(sdump);
-        this.digest(dump);
-        this.digestSkynet(sdump);
+        console.log(Papa_Dump.PlayerScores);
+        this.digest(Papa_Dump.PlayerScores);
+        this.giveBlueBaseStrength(Papa_Dump)
     }
 
+    //player data
     digest = (d) => {
         let formatted = [];
-        Object.keys(d).map((key, index) => {
+        Object.keys(d).forEach((key) => {
             formatted.push({name: key, ...d[key]})
         })
         this.setState({dataDump: formatted});
     }
 
-    giveBlueBaseStrength = (a) => {
-        let baseDict = a["bspaceData"]["blue"]["base"];
-        let formatBaseList = [];
-        Object.keys(baseDict).map( (key, index) => {
+    giveBlueBaseStrength = (data) => {
+        let baseDict = data["BaseData"]["blueCoalition"]["baseData"]
+        let formatedBaseList = [];
+        Object.keys(baseDict).forEach((key) => {
+            console.log(key);
             let unitList = [];
-            let unordersUnitList = baseDict[key]["types"]
-            console.log("Inside Map")
-            console.log(unordersUnitList)
-            Object.keys(unordersUnitList).map( (key, index) =>{
+            let unorderedUnitList = baseDict[key]["type"]
+            Object.keys(unorderedUnitList).forEach( (key) =>{
                 unitList.push({
                     name: key,
-                    amt: unordersUnitList[key]
+                    amt: unorderedUnitList[key]
                 })
             } )
             
-            formatBaseList.push({
+            formatedBaseList.push({
                 name: key,
                 amt: baseDict[key]["str"],
                 children: unitList
             })
         })
+        console.log(formatedBaseList);
         let replacement = {
             "name": "xsaf",
                 "color": "hsl(27, 70%, 50%)",
-                "children": formatBaseList
+                "children": formatedBaseList
                 }
         this.setState({
                 chartMotionDamp: 2,
@@ -102,7 +100,7 @@ class App extends Component {
             children: []
         };
         let count = 0;
-        Object.keys(dynadata).map(function(key, index) {
+        Object.keys(dynadata).forEach((key) => {
             replacement["children"].push( 
                 {
                     name: key,
@@ -120,22 +118,6 @@ class App extends Component {
             chartCircleLines: false,
             charCircleColors: colorSchemes["fuel"]
         })
-    }
-
-    digestSkynet = (d) => {
-        let formatted = [];
-        Object.keys(d).map((key, index) => {
-            formatted.push({name: key, ...d[key]})
-        })
-        this.setState({skynetDump: formatted});
-    }
-    
-    digestBases = (d) => {
-        let formatted = [];
-        Object.keys(d).map((key, index) => {
-            formatted.push({name: key, ...d[key]})
-        })
-        this.setState({skynetDump: formatted});
     }
 
     orderByKills = () => {
@@ -184,8 +166,8 @@ class App extends Component {
                 pname = entry["name"];
                 pdict = entry;  
                 ret.push(
-                    <div className="card">
-                        <div className="card-body">
+                    <Card>
+                        <CardBody>
                             <h3 className="card-title">{pname}</h3>
                             <h5 className="card-sub-title mb-2 text-muted">Flight Time: {pdict["SeatTime"]}</h5>
                             <div className="card-section">
@@ -210,8 +192,8 @@ class App extends Component {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        </CardBody>
+                    </Card>
                 )
             }    
         })
@@ -261,20 +243,7 @@ class App extends Component {
         )
     }
 
-    TitleBar = () => {
-        return (
-            <div className="title-group">
-                <div className="title site-title">
-                    <h1>[XSAF] Camelot Dynamic Server</h1>
-                </div>
-                <div className="title site-title">
-                    <h1>Server Data</h1>
-                </div>
-            </div>
-        )
-    }
-
-    render() {
+    render = () => {
         return (
             <main className="content">
                 {this.pageTitleBar()}
@@ -285,7 +254,7 @@ class App extends Component {
                                 <h3>Airfield Data</h3>
                             </div>
                             <div className="btn-mygroup">
-                                <button className="my-btn btn btn-info" onClick={() => this.giveBlueBaseStrength(sdump)}>Blue Strength</button>
+                                <button className="my-btn btn btn-info" onClick={() => this.giveBlueBaseStrength(Papa_Dump)}>Blue Strength</button>
                                 <button className="my-btn btn btn-info" onClick={() => this.giveBaseFuel()}>Blue Fuel</button>
                             </div>
                             <div className="first">
@@ -297,14 +266,14 @@ class App extends Component {
                                 <h3>Player Data</h3>
                             </div>
                             <div className="btn-mygroup">
-                                <button className="my-btn btn btn-primary" onClick={() => this.digest(dump)}>Refresh Data</button>
+                                <button className="my-btn btn btn-primary" onClick={() => this.digest(Papa_Dump.PlayerScores)}>Refresh Data</button>
                                 <button className="my-btn btn btn-info" onClick={() => this.orderByKDR()}>Kill/Death</button>
                                 <button className="my-btn btn btn-success" onClick={() => this.orderByKills()}>Kills</button>
                                 <button className="my-btn btn btn-danger" onClick={() => this.orderByDeaths()}>Deaths</button>
                                 <button className="my-btn btn btn-info" onClick={() => this.orderByTime()}>Time</button>
                                 <button class="my-btn btn btn-secondary" onClick={() => this.toggle()}>Search</button>
                             </div>
-                            <div className="nexttest player-data example">
+                            <div className="nexttest player-data no-scrollbar">
                                 {this.makeFlightData()}
                             </div>
                         </div>
