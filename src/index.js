c6d8d3bd2639674c5baec0a7ad6383b064165a9b
distant <oscar.rodr031@gmail.com>
2021-02-19 07:10:36 -0500

    updates with routing and structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import App from './app.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
